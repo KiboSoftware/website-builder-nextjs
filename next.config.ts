bc0538b13c900f39106467b3a2f665d23ae88cdb
kevin <kevin.watts@kibocommerce.com>
2025-07-18 11:08:10 -0500

    add cross origin header

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
                     // this will allow your website to be framed under the specified domains for live editing
                     {
                         key: "Content-Security-Policy",
-                        value: "frame-ancestors http://localhost:3001"
+                        value: "frame-ancestors http://localhost:3000 http://localhost:3001 https://d3fak6u4cx01ke.cloudfront.net https://d3fyk9u8dx02bn.cloudfront.net"
                         // Example: "frame-ancestors http://localhost:3001 https://d3fak6u4cx01ke.cloudfront.net"
                     }
                 ]
